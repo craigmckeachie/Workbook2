f8efd2fd5c0ea5d66940f5b5678cbb4eba813149
Craig McKeachie <craigdmckeachie@gmail.com>
2022-09-28 15:30:51 -0400

    making progress

--- HelloWorld/hello.js
+++ HelloWorld/hello.js
@@ -1,4 +1,10 @@
 "use strict";
 
-var message = "Hello World!";
-console.log(message);
+var x = "123";
+console.log(typeof x);
+x = Number(x);
+console.log(typeof x);
+var y = "456";
+y = Number(y);
+var z = x + y;
+console.log(z);
